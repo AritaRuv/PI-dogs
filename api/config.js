3a00c75c4f8d1f40d18a1aa53af48c1293675a75
AritaRuv <ariadnaruvini@hotmail.com.ar>
2022-10-20 12:48:00 -0300

    config

--- api/config.js
+++ api/config.js
@@ -4,4 +4,4 @@ DB_HOST= process.env.DB_HOST || 'localhost'
 DB_PORT= process.env.DB_PORT || 3306
 PORT =  process.env.PORT || 3001
 
-modules.export = { DB_HOST, DB_PASSWORD, DB_USER, DB_PORT, PORT}
+module.exports = { DB_HOST, DB_PASSWORD, DB_USER, DB_PORT, PORT}
